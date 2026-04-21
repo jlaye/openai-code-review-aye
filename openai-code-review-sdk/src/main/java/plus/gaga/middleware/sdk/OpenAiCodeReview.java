8f10588a01d01c5e52ea68e28fc08b65ed791e1f
jlsn_ysf <jlsn_ysf@noreply.gitcode.com>
2026-04-21 21:27:26 +0800

    feat: 提交检出代码

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,7 +1,30 @@
 package plus.gaga.middleware.sdk;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class OpenAiCodeReview {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
+        //1.代码检出
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD-1", "HEAD");
+        //制定目录
+        ProcessBuilder directory = processBuilder.directory(new File("."));
+        //执行
+        Process process = processBuilder.start();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder diffCode = new StringBuilder();
+        while ((line = bufferedReader.readLine()) !=null){
+            diffCode.append(line);
+        }
+        int exitCode = process.waitFor();
+        System.out.println("Exited with code = " + exitCode);
+        System.out.println("diffCode.toString() = " + diffCode);
+
+
     }
+
 }
