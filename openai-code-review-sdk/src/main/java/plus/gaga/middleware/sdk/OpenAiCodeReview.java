45bb60871a76472e49949f51e4385b56ce0515c1
jlsn_ysf <jlsn_ysf@noreply.gitcode.com>
2026-04-21 21:40:29 +0800

    feat: HEAD-1 写错了应该是HEAD~1

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -8,14 +8,15 @@ public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
         //1.代码检出
-        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD-1", "HEAD");
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
         //制定目录
-        ProcessBuilder directory = processBuilder.directory(new File("."));
+        processBuilder.directory(new File("."));
         //执行
         Process process = processBuilder.start();
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
+
         StringBuilder diffCode = new StringBuilder();
         while ((line = bufferedReader.readLine()) !=null){
             diffCode.append(line);
