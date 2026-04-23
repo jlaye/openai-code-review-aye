f2f0a23b7fbd9b24911e4bb0a0d077b0fbb5c4cd
jlsn_ysf <jlsn_ysf@noreply.gitcode.com>
2026-04-23 22:15:31 +0800

    feat：切换一个token

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -59,7 +59,7 @@ public class OpenAiCodeReview {
 
     private static String codeReview(String diffCode) throws Exception {
 
-        String apiKeySecret = "c78fbacd3e10118ad5649d7a54a3a163.UunYDBxpzeClvSKZ";
+        String apiKeySecret = "d4946eef67084eb29162a4c4f7c24961.rKibnmDHtCne2ScF";
         String token = BearerTokenUtils.getToken(apiKeySecret);
 
         URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
