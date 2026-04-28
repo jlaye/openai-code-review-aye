692069bdb8a6c4bb44dd81ff4616ca8cbf3171fd
jlsn_ysf <jlsn_ysf@noreply.gitcode.com>
2026-04-28 19:39:04 +0800

    feat : v6.1

--- openai-code-review-test/src/test/java/test/ApiTest.java
+++ openai-code-review-test/src/test/java/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("zzzzzz"));
+        System.out.println(Integer.parseInt("new test"));
     }
 
 }
