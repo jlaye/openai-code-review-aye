c10ee3499aff39e1e40d3b041f3483f05564ecd4
jlsn_ysf <jlsn_ysf@noreply.gitcode.com>
2026-04-30 20:42:40 +0800

    feat:第七届 重构代码 v7.2

--- openai-code-review-test/src/test/java/test/ApiTest.java
+++ openai-code-review-test/src/test/java/test/ApiTest.java
@@ -13,7 +13,7 @@ public class ApiTest {
 
     @Test
     public void test() {
-        System.out.println(Integer.parseInt("11111111111"));
+        System.out.println(Integer.parseInt("123"));
     }
 
 }
